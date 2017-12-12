7ee3194287f952663cb3e1677b6594a81564696e
owner <owner@owner-PC>
2017-12-12 13:35:23 +0900

    third commit

--- test_git/src/kr/co/sist/test/TestGit.java
+++ test_git/src/kr/co/sist/test/TestGit.java
@@ -6,6 +6,11 @@ public class TestGit {
 		for(int i =0; i<10; i++) {
 			System.out.println(i + " : Test git");
 		}
+		System.out.println("수정수정수정");
+		System.out.println("수정수정수정");
+		System.out.println("수정수정수정");
+		System.out.println("수정수정수정");
+		
 	}
 
 }
